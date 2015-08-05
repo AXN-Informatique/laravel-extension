3a2731d0bb830d4ae4a7e1dac2bafe4d0d458ba9
Lucas Girardot <lucas.girardot@axn.fr>
2015-08-05 10:02:10 +0200

    Suppression de l'extension de la relation MorphTo.

--- changelog.md
+++ changelog.md
@@ -1,5 +1,10 @@
 # Changelog for Laravel Extension
 
+## 1.0.1 (2015-08-05)
+
+- Suppression de l'extension de la relation MorphTo qui posait problème pour les modèles
+  se trouvant dans des sous-dossiers.
+
 ## 1.0.0 (2015-07-20)
 
 - First release.
--- src/Database/Eloquent/Model.php
+++ src/Database/Eloquent/Model.php
@@ -2,9 +2,7 @@
 
 namespace Axn\Illuminate\Database\Eloquent;
 
-use ReflectionClass;
 //use Illuminate\Database\Eloquent\Model as BaseModel;
-use Axn\Illuminate\Database\Eloquent\Relations\MorphTo;
 
 trait Model /*extends BaseModel*/
 {
@@ -18,50 +16,5 @@ trait Model /*extends BaseModel*/
     {
         // Axn\Illuminate\Database\Eloquent\Builder
         return new Builder($query);
-    }
-
-    /**
-     * Définit l'inverse d'une relation polymorphique en prenant en compte le fait
-     * que le morph type ne contient pas le namespace du modèle.
-     *
-     * @param  string  $name
-     * @param  string  $type
-     * @param  string  $id
-     * @return \Axn\Illuminate\Database\Eloquent\Relations\MorphTo
-     */
-    public function morphTo($name = null, $type = null, $id = null)
-    {
-        if (is_null($name)) {
-            list(, $caller) = debug_backtrace(false);
-
-            $name = snake_case($caller['function']);
-        }
-
-        list($type, $id) = $this->getMorphs($name, $type, $id);
-
-        if (is_null($class = $this->$type)) {
-            // Axn\Illuminate\Database\Eloquent\Relations\MorphTo
-            return new MorphTo(
-                $this->newQuery(), $this, $id, null, $type, $name
-            );
-        } else {
-            $class = (new ReflectionClass($this))->getNamespaceName().'\\'.$class;
-            $instance = new $class;
-
-            // Axn\Illuminate\Database\Eloquent\Relations\MorphTo
-            return new MorphTo(
-                with($instance)->newQuery(), $this, $id, $instance->getKeyName(), $type, $name
-            );
-        }
-    }
-
-    /**
-     * Retourne le nom de classe pour la relation polymorphique.
-     *
-     * @return string
-     */
-    public function getMorphClass()
-    {
-        return $this->morphClass ?: class_basename($this);
     }
 }
