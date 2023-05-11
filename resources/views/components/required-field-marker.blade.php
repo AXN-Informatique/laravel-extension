b2d0090013b4d31135f6167793078f8ae7261e66
Vincent Garnier <forxer@gmail.com>
2023-05-11 09:33:33 +0200

    New fix for loading components views

--- resources/views/components/required-field-marker.blade.php
+++ resources/views/components/required-field-marker.blade.php
@@ -1,3 +1,3 @@
 <span {{ $attributes->merge(['class' => 'required-field-marker']) }}>
-    {{ $symbol }}<span>{{ trans('misc.required') }}</span>
+    {!! $symbol !!}<span>{{ trans('misc.required_field') }}</span>
 </span>
